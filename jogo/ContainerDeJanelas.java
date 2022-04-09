6775137b44c61ec1a94841adeb15ff04428bfa0b
Gabriel-Delazeri <gbldelazeri@gmail.com>
2022-04-08 23:31:53 -0300

    fase feita

--- jogo/ContainerDeJanelas.java
+++ jogo/ContainerDeJanelas.java
@@ -1,14 +1,23 @@
+// import das bibliotecas
+package jogo;
+
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
+// classe que vai gerar a janela do jogo
+
 public class ContainerDeJanelas extends JFrame{
+
+    // constructor 
 	public ContainerDeJanelas() {
 		
+        
 		add(new JButton("Teste"));
 		setTitle("Jogo APS");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,400);
+		setSize(1000,1000);
+        setResizable(false);
 		setVisible(true);
 	}
 	
